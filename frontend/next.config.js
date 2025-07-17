5c2f3dc7d24c8c5197f57bcba133c4f48ccdb45c
Emmanuel Russel <emmanuel@Emmanuels-MacBook-Pro.local>
2025-07-18 00:29:21 +0400

    Updated next.config.js for Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,35 +9,6 @@ const nextConfig = {
       domains: ['images.unsplash.com', 'logos-download.com'],
       formats: ['image/avif', 'image/webp'],
     },
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-        },
-      ]
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'X-Frame-Options',
-              value: 'DENY',
-            },
-            {
-              key: 'X-XSS-Protection',
-              value: '1; mode=block',
-            },
-          ],
-        },
-      ]
-    },
   }
   
   module.exports = nextConfig
